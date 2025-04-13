e5cb826a95ee99b623d68286f964777a9f4050fa
Ján Mokrý <jancimokry@gmail.com>
2025-04-13 21:01:49 +0200

    03 - Validating User Input

--- myfancypdfinvoices/src/main/java/sk/janmokry/context/MyFancyPdfInvoicesApplicationConfiguration.java
+++ myfancypdfinvoices/src/main/java/sk/janmokry/context/MyFancyPdfInvoicesApplicationConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.validation.beanvalidation.MethodValidationPostProcessor;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import sk.janmokry.ApplicationLauncher;
 
@@ -15,6 +16,12 @@ import sk.janmokry.ApplicationLauncher;
         , ignoreResourceNotFound = true)
 @EnableWebMvc
 public class MyFancyPdfInvoicesApplicationConfiguration {
+
+    @Bean //
+    public MethodValidationPostProcessor methodValidationPostProcessor() {
+        return new MethodValidationPostProcessor();
+    }
+
 
     @Bean
     public ObjectMapper objectMapper() {
