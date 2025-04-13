2416d8c18c205676ce22ba500c80e8529be8492f
Ján Mokrý <jancimokry@gmail.com>
2025-04-13 16:39:05 +0200

    03 - WebMVC Hello World

---
+++ myfancypdfinvoices/src/main/java/sk/janmokry/web/MyFirstSpringController.java
@@ -0,0 +1,15 @@
+package sk.janmokry.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MyFirstSpringController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String index() {
+        return "Hello World";
+    }
+}
