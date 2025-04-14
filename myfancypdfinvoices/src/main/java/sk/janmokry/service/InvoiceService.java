f2517041ea3ca8d6d255219d531468806c7fe9ad
Ján Mokrý <jancimokry@gmail.com>
2025-04-14 12:51:44 +0200

    05 - @Transactional

--- myfancypdfinvoices/src/main/java/sk/janmokry/service/InvoiceService.java
+++ myfancypdfinvoices/src/main/java/sk/janmokry/service/InvoiceService.java
@@ -7,6 +7,8 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
 import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 import sk.janmokry.model.Invoice;
 
 import java.sql.PreparedStatement;
@@ -41,7 +43,9 @@ public class InvoiceService {
         // TODO actual deletion of PDFs
     }
 
+    @Transactional
     public List<Invoice> findAll() {
+        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
         return jdbcTemplate.query("select id, user_id, pdf_url, amount from invoices", (resultSet, rowNum) -> {
             Invoice invoice = new Invoice();
             invoice.setId(resultSet.getObject("id").toString());
@@ -52,7 +56,9 @@ public class InvoiceService {
         });
     }
 
+    @Transactional
     public Invoice create(String userId, Integer amount) {
+        System.out.println("Is a database transaction open? = " + TransactionSynchronizationManager.isActualTransactionActive());
         String generatedPdfUrl = cdnUrl + "/images/default/sample.pdf";
 
         KeyHolder keyHolder = new GeneratedKeyHolder();
