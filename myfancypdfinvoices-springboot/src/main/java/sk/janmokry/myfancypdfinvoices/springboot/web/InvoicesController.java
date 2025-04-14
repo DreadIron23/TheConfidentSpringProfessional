43ebdf380a885791c71bf9737b2a755964e81cd9
Ján Mokrý <jancimokry@gmail.com>
2025-04-14 16:17:34 +0200

    06 - REST Services

---
+++ myfancypdfinvoices-springboot/src/main/java/sk/janmokry/myfancypdfinvoices/springboot/web/InvoicesController.java
@@ -0,0 +1,33 @@
+package sk.janmokry.myfancypdfinvoices.springboot.web;
+
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import sk.janmokry.myfancypdfinvoices.springboot.dto.InvoiceDto;
+import sk.janmokry.myfancypdfinvoices.springboot.model.Invoice;
+import sk.janmokry.myfancypdfinvoices.springboot.service.InvoiceService;
+
+import java.util.List;
+
+@RestController
+public class InvoicesController {
+
+    private final InvoiceService invoiceService;
+
+    public InvoicesController(InvoiceService invoiceService) {
+        this.invoiceService = invoiceService;
+    }
+
+    @GetMapping("/invoices")
+    // @RequestMapping(value = "/invoices", method = RequestMethod.GET)
+    public List<Invoice> invoices() {
+        return invoiceService.findAll();
+    }
+
+    @PostMapping("/invoices")
+    public Invoice createInvoice(@Valid @RequestBody InvoiceDto invoiceDto) {
+        return invoiceService.create(invoiceDto.getUserId(), invoiceDto.getAmount());
+    }
+}
